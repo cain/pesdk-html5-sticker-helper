0509a93a2da939fdda23ddae2490803265fe47f8
Cain Hall <cain.hall98@gmail.com>
2021-09-30 09:30:57 +1000

    Update to v5 pesdk

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # photoeditor-sticker-helper
+
+Creates json files to help create custom stickers for [PhotoEditorSdk](https://img.ly/docs/pesdk/web/features/stickers/) v5
+
+Simply add a folders of images inside the /image folder and update constants in index.js
--- index.js
+++ index.js
@@ -1,39 +1,33 @@
-const sizeOf = require('image-size');
 const fs = require('fs');
 const _ = require('lodash');
 
-// Uri for main repo
-const uri = 'stickers/custom';
-
-// Default stickers
-const stickers = ["imgly_sticker_shapes_badge_04", "imgly_sticker_shapes_arrow_02", "imgly_sticker_shapes_arrow_03", "imgly_sticker_shapes_badge_01", "imgly_sticker_shapes_badge_04", "imgly_sticker_shapes_badge_06", "imgly_sticker_shapes_badge_08", "imgly_sticker_shapes_badge_11", "imgly_sticker_shapes_badge_19", "imgly_sticker_shapes_badge_5", "imgly_sticker_shapes_badge_12", "custom_sticker_cactus_1"];
+const URI = 'custom';
+const RESIZE_MODE = 'keepAspect';
+const TINT_MODE = 'colorized';
+const OUTPUT_FILE_TYPE = '.svg';
 
 // Sticker packs
 const packs = [
   {
     name: 'space',
-    folder: './images/Space Pack 1/Space PNGs/'
+    folder: './images/Space Pack 1/'
   },
   {
     name: 'garden',
-    folder: './images/Garden Pack 1/Garden PNGs/'
+    folder: './images/Garden Pack 1/'
   },
   {
     name: 'cactus',
-    folder: './images/Cactus Pack 1/Cactus PNGs/'
+    folder: './images/Cactus Pack 1/'
   }
 ];
 
-const data = { availableStickers: stickers };
-
 packs.forEach((pack) => {
-  const cat = {
+  const item = {
     "identifier": pack.name,
-    "defaultName": _.capitalize(pack.name),
-    "metaData": {
-      "backgroundImage": "stickers/custom/background.png"
-    },
-    "stickers": []
+    "name": _.capitalize(pack.name),
+    "thumbnailURI": "/custom/background.png",
+    "items": [],
   };
 
   // Read files in pack (ignore hidden files like .DS_Store)
@@ -41,40 +35,24 @@ packs.forEach((pack) => {
     .filter(item => !(/(^|\/)\.[^\/\.]/g).test(item))
     .forEach((fileName) => {
       const identifier = fileName.replace(' ', '');
-      const dimensions = sizeOf(pack.folder + fileName);
 
       // Construct full file uri
-      const fileURI = `${uri}/${pack.name}/${identifier.replace('.png', '.svg')}`;
+      const fileURI = `${URI}/${pack.name}/${identifier.replace('.png', OUTPUT_FILE_TYPE)}`;
 
-      data.availableStickers.push(identifier);
-      cat.stickers.push({
+      item.items.push({
         "identifier": identifier,
-        "defaultName": _.capitalize(fileName),
-        "images": {
-          "mediaBase": {
-            "uri": fileURI,
-            "width": dimensions.width,
-            "height": dimensions.height
-          },
-          "mediaThumb": {
-            "uri": fileURI,
-            "width": 50,
-            "height": 50
-          }
-        }
+        "name": _.capitalize(fileName),
+        "stickerURI": fileURI,
+        "thumbnailURI": fileURI,
+        "resizeMode": RESIZE_MODE,
+        "tintMode": TINT_MODE,
       })
     })
 
   // Stringify json with correct format
-  const json = JSON.stringify(cat, null, 2);
+  const json = JSON.stringify(item, null, 2);
 
   // Write to stick pack json file
   fs.writeFileSync('./json/'+pack.name+'-stickers.json', json);
   console.log('Created json file ' + pack.name + '-stickers.json')
 })
-
-// Stringify json with correct format
-const json = JSON.stringify(data, null, 2);
-
-// Write to json file
-fs.writeFileSync('data.json', json);
